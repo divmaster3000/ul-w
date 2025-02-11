6379aeebbe0f70c4c368a19ab3e84dcf5e92996c
Max A <corp.workMA@gmail.com>
2025-02-11 23:05:57 +0200

    add chakra ui configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,9 @@
 import { createRoot } from "react-dom/client";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <ChakraProvider value={defaultSystem}>
+    <App />
+  </ChakraProvider>
+);
