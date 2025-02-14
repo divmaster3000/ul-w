cdfbc6debd5f2c0a09e5d87c670ae66ddc9f28e4
Max A <corp.workMA@gmail.com>
2025-02-14 22:28:51 +0200

    add text styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import { Button } from "@chakra-ui/react";
 function App() {
   return (
     <>
-      <Button color="{colors.neutral.50}" background="{colors.primary.400}">
+      <Button
+        color="{colors.neutral.50}"
+        background="{colors.primary.400}"
+        textStyle="h6"
+      >
         Theme
       </Button>
     </>
--- src/theme/Theme.tsx
+++ src/theme/Theme.tsx
@@ -1,4 +1,124 @@
-import { createSystem, defineConfig } from "@chakra-ui/react";
+import { createSystem, defineConfig, defineTextStyles } from "@chakra-ui/react";
+
+const DEFAULT_TEXT_STYLES = defineTextStyles({
+  h1: {
+    value: {
+      fontFamily: "Lato",
+      fontSize: "96px",
+      lineHeight: "112px",
+      fontWeight: "300",
+      letterSpacing: "-1.5px",
+    },
+  },
+  h2: {
+    value: {
+      fontFamily: "Lato",
+      fontSize: "60px",
+      lineHeight: "72px",
+      fontWeight: "400",
+      letterSpacing: "-0.5px",
+    },
+  },
+  h3: {
+    value: {
+      fontFamily: "Lato",
+      fontSize: "48px",
+      lineHeight: "56px",
+      fontWeight: "400",
+      letterSpacing: "0",
+    },
+  },
+  h4: {
+    value: {
+      fontFamily: "Lato",
+      fontSize: "34px",
+      lineHeight: "42px",
+      fontWeight: "500",
+      letterSpacing: "0.25px",
+    },
+  },
+  h5: {
+    value: {
+      fontFamily: "Lato",
+      fontSize: "24px",
+      lineHeight: "32px",
+      fontWeight: "500",
+      letterSpacing: "0",
+    },
+  },
+  h6: {
+    value: {
+      fontFamily: "Lato",
+      fontSize: "20px",
+      lineHeight: "32px",
+      fontWeight: "500",
+      letterSpacing: "0.15px",
+    },
+  },
+  subtitle1: {
+    value: {
+      fontFamily: "Lato",
+      fontSize: "16px",
+      lineHeight: "28px",
+      fontWeight: "500",
+      letterSpacing: "0.15px",
+    },
+  },
+  button1: {
+    value: {
+      fontFamily: "Lato",
+      fontSize: "16px",
+      lineHeight: "28px",
+      fontWeight: "500",
+      letterSpacing: "0.15px",
+    },
+  },
+  subtitle2: {
+    value: {
+      fontFamily: "Lato",
+      fontSize: "14px",
+      lineHeight: "22px",
+      fontWeight: "500",
+      letterSpacing: "0.1px",
+    },
+  },
+  button2: {
+    value: {
+      fontFamily: "Lato",
+      fontSize: "14px",
+      lineHeight: "22px",
+      fontWeight: "500",
+      letterSpacing: "0.1px",
+    },
+  },
+  body1: {
+    value: {
+      fontFamily: "Lato",
+      fontSize: "16px",
+      lineHeight: "24px",
+      fontWeight: "400",
+      letterSpacing: "0.15px",
+    },
+  },
+  body2: {
+    value: {
+      fontFamily: "Lato",
+      fontSize: "14px",
+      lineHeight: "20px",
+      fontWeight: "400",
+      letterSpacing: "0.15px",
+    },
+  },
+  caption: {
+    value: {
+      fontFamily: "Lato",
+      fontSize: "12px",
+      lineHeight: "20px",
+      fontWeight: "400",
+      letterSpacing: "0.4px",
+    },
+  },
+});
 
 const COMMON_PALETTE = {
   primary: {
@@ -83,6 +203,7 @@ const lightTheme = defineConfig({
         },
       },
     },
+    textStyles: DEFAULT_TEXT_STYLES,
   },
 });
 
@@ -106,6 +227,7 @@ const darkTheme = defineConfig({
         },
       },
     },
+    textStyles: DEFAULT_TEXT_STYLES,
   },
 });
 
