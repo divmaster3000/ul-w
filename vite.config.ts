4903d20d287bb50bfba20eb7f3d08c342ec68227
Max A <corp.workMA@gmail.com>
2025-02-17 22:22:07 +0200

    add initial routing configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+// eslint-disable-next-line import/no-unresolved
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    TanStackRouterVite({ autoCodeSplitting: true }),
+  ],
 });
